b2854ada7a113a7e08d77945c5c2b6ed816cc83e
YUGANT <yugantloya@gmail.com>
2023-10-12 15:43:45 +0530

    Ui Changed and Word Finder using Letters Created and Dict Scriptable Created for Optimization

--- Assets/FindWords/Scripts/WordLengthFinder.cs
+++ Assets/FindWords/Scripts/WordLengthFinder.cs
@@ -1,16 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using UnityEngine;
-using UnityEditor;
 using NaughtyAttributes;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class WordLengthFinder : MonoBehaviour
 {
     public int wordLength = 3;
     public char letter;
     public TextAsset oxfordDictTextFile, wordLengthTextFile;
+    public MainDictionary mainDict;
+
+    [Tooltip("Minimum and Maximum Folder or word Length of which you have to add all data !")]
+    public Vector2Int minMaxScriptableWordLength;
 
     [Button]
     public void FindWordLength()
@@ -32,7 +40,7 @@ public class WordLengthFinder : MonoBehaviour
         string assetsFolderPath = Application.dataPath;
 
         // Specify the relative path within the "Assets" folder where you want to save the CSV file
-        string relativeFilePath = $"FindWords/CSV/WordLengthDictionary/WordLength_{wordLength}.csv";
+        string relativeFilePath = $"FindWords/Resources/WordDictData/WordLength_{wordLength}.csv";
 
         // Combine the paths to get the full path of the CSV file
         string filePath = Path.Combine(assetsFolderPath, relativeFilePath);
@@ -67,50 +75,102 @@ public class WordLengthFinder : MonoBehaviour
         string data = wordLengthTextFile.text.Trim();
         string[] lines = data.Split('\n');
 
-        // for (int i = 65; i <= 91; i++)
-        // {
-        //     char letter = (char)i;
-        StringBuilder csvContent = new StringBuilder();
-        for (int j = 0; j < lines.Length; j++)
+        //char currLetter = letter;
+
+        for (int i = 65; i < 91; i++)
         {
-            if (!string.IsNullOrWhiteSpace(lines[j]) && lines[j][0].ToString().ToUpper() == letter.ToString().ToUpper())
+            char currletter = (char)i;
+            StringBuilder csvContent = new StringBuilder();
+            foreach (var str in lines)
             {
-                csvContent.AppendLine(lines[j]);
+                if (!string.IsNullOrWhiteSpace(str) &&
+                    str[0].ToString().ToUpper() == currletter.ToString().ToUpper())
+                {
+                    csvContent.AppendLine(str.Trim());
+                }
             }
-        }
 
-        string assetsFolderPath = Application.dataPath;
+            string assetsFolderPath = Application.dataPath;
 
-        // Specify the relative path within the "Assets" folder where you want to save the CSV file
-        string relativeFilePath =
-            $"FindWords/CSV/WordLengthDictionary/{wordLength}_WordLengthFolder/{letter.ToString().ToUpper()}_Words.csv";
+            // Specify the relative path within the "Assets" folder where you want to save the CSV file
+            string relativeFilePath =
+                $"FindWords/Resources/WordDictData/{wordLength}_WordLengthFolder/{currletter.ToString().ToUpper()}_Words.csv";
 
-        // Combine the paths to get the full path of the CSV file
-        string filePath = Path.Combine(assetsFolderPath, relativeFilePath);
+            // Combine the paths to get the full path of the CSV file
+            string filePath = Path.Combine(assetsFolderPath, relativeFilePath);
 
-        // Save the CSV file
+            // Save the CSV file
 
-        if (!string.IsNullOrEmpty(filePath) && !File.Exists(filePath))
-        {
-            Debug.Log("File Creating !!");
-            File.WriteAllText(filePath, csvContent.ToString());
-        }
-        else
-        {
-            Debug.LogError($"File Already Exists with name WordLength_{wordLength} !!");
-        }
+            if (!string.IsNullOrEmpty(filePath) && !File.Exists(filePath))
+            {
+                Debug.Log("File Creating !!");
+                File.WriteAllText(filePath, csvContent.ToString());
+            }
+            else
+            {
+                Debug.LogError($"File Already Exists with name WordLength_{wordLength} !!");
+            }
 
-        if (!Directory.Exists(filePath))
-        {
-            Debug.Log("Folder Creating !");
-            Directory.CreateDirectory(Path.GetDirectoryName(filePath));
-        }
+            if (!Directory.Exists(filePath))
+            {
+                Debug.Log("Folder Creating !");
+                Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+            }
 
-        // Refresh the Unity Asset Database to make the file visible in the Editor
+            // Refresh the Unity Asset Database to make the file visible in the Editor
 #if UNITY_EDITOR
-        AssetDatabase.Refresh();
+            AssetDatabase.Refresh();
 #endif
-        Debug.Log("CSV file saved to: " + filePath);
+            Debug.Log("CSV file saved to: " + filePath);
+        }
     }
-    //}
+
+    [Button]
+    void AddAllFilesToScriptable()
+    {
+        MainDictionary dict = mainDict;
+
+        List<MainDictionary.MainDictionaryInfo> dictInfoList = new List<MainDictionary.MainDictionaryInfo>();
+
+        for (int i = 0; i <= minMaxScriptableWordLength.y - minMaxScriptableWordLength.x; i++)
+        {
+            MainDictionary.MainDictionaryInfo mainDictionaryInfo = new MainDictionary.MainDictionaryInfo();
+            //mainDictionaryInfo = dict.dictInfoList[i];
+
+
+            for (int j = minMaxScriptableWordLength.x; j <= minMaxScriptableWordLength.y; j++)
+            {
+                List<MainDictionary.WordLengthDetailedInfo> dictWordInfoList =
+                    new List<MainDictionary.WordLengthDetailedInfo>();
+
+                for (int k = 65; k < 91; k++)
+                {
+                    MainDictionary.WordLengthDetailedInfo wordInfo = new MainDictionary.WordLengthDetailedInfo();
+
+                    char tempChar = (char)k;
+                    Debug.Log("File Creating !!");
+
+                    TextAsset textFile =
+                        Resources.Load<TextAsset>($"WordDictData/{j}_WordLengthFolder/{tempChar}_Words");
+
+                    wordInfo.wordText = textFile;
+                    wordInfo.wordStartChar = tempChar;
+
+                    dictWordInfoList.Add(wordInfo);
+                }
+
+                mainDictionaryInfo.wordLength = minMaxScriptableWordLength.x + i;
+                mainDictionaryInfo.wordsInfo = dictWordInfoList;
+            }
+
+            dictInfoList.Add(mainDictionaryInfo);
+            dict.dictInfoList = dictInfoList;
+            mainDict = dict;
+
+#if UNITY_EDITOR
+            EditorUtility.SetDirty(mainDict);
+            AssetDatabase.Refresh();
+#endif
+        }
+    }
 }
