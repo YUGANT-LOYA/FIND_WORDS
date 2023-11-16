3ec1ad06dfe7dc393451f0b8b57a251a4636ed72
YUGANT <yugantloya@gmail.com>
2023-11-16 12:48:40 +0530

    Iq Bar Starting from 1

--- Assets/FindWords/Scripts/GameController.cs
+++ Assets/FindWords/Scripts/GameController.cs
@@ -22,7 +22,7 @@ namespace YugantLoyaLibrary.FindWords
 
         [Range(4, 7)] public int startingGridSize = 5;
         [HideInInspector] public int startingQuesSize = 3, maxGridSize = 7;
-        public int defaultIq, changeBgAfter = 3;
+        public int defaultIq = 1, changeBgAfter = 3;
 
         [Header("References")] [SerializeField]
         private PickWordDataInfo pickWordDataInfo;
